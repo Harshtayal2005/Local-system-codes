5f087eda7a87c7da4b578d956cfc4cee1a0706a3
Harsh Tayal <harshtayal2005@gmail.com>
2023-12-14 12:03:25 +0530

    count sort modified for stable algo

--- Sorting/countSort.cpp
+++ Sorting/countSort.cpp
@@ -2,26 +2,38 @@
 using namespace std;
 
 
-void countSort(int arr[], int n){
-    int freqArr[10]={0};
-    for(int i=0; i<n; i++){
-        freqArr[arr[i]]++;
-    }
-    int k=0;
-    for(int j=0; j<10; j++){
-        while(freqArr[j]--){
-            arr[k++] = j;
-        }
-    }
+vector<int> countSort(vector<int> arr, int n, int maxEle){
+    vector<int> count(maxEle + 1, 0);
     
+    //storing the count of each element in count array
+    for(auto& i : arr){
+        count[i]++;
+    }
+
+    //finding the cumilative or prefix sum of count arraywhich tells how many elements present at index i 
+    // are less than or equal to i
+    for (int i = 1; i <= maxEle; i++){
+        count[i] += count[i - 1];
+    }
+
+    // traversing from right to left in input array to maintain the relative order of equal elements
+    vector<int> output = arr;
+
+    for (int i = n-1; i >= 0; i--){
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    return output;
 }
 
-
 int main(){
-    int arr[]={9,2,3,5,1,0,3};
-    countSort(arr, 7);
-    for(int ele:arr){
-        cout<<ele<<" ";
+    vector<int> arr={9,2,3,5,1,0,3};
+
+    vector<int>sortedarr = countSort(arr, 7, 9);
+
+    for(auto& i : sortedarr){
+        cout<<i<<" ";
     }
     cout<<endl;
     return 0;
--- Sorting/countSortstring.cpp
+++ Sorting/countSortstring.cpp
@@ -42,3 +42,36 @@ int main(){
     cout<<endl;
     return 0;
 }
+
+//stable count sort string
+/*class Solution
+{
+public:
+    // Function to arrange all letters of a string in lexicographical
+    // order using Counting Sort.
+    string countSort(string arr)
+    {
+        vector<int> count(26, 0);
+
+        for (auto &i : arr)
+        {
+            count[i - 'a']++;
+        }
+
+        for (int i = 1; i < 26; i++)
+        {
+            count[i] += count[i - 1];
+        }
+
+        string ans = arr;
+
+        for (int i = arr.length() - 1; i >= 0; i--)
+        {
+            ans[count[arr[i] - 'a'] - 1] = arr[i];
+            count[arr[i] - 'a']--;
+        }
+
+        return ans;
+    }
+};
+*/
