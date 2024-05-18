ab21b0bfaf3352f938fa5671a72232504787590d
Harsh Tayal <harshtayal2005@gmail.com>
2024-05-18 17:29:59 +0530

    Selection sort modified

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,24 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[i]){
-                swap(arr[j], arr[i]);
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int mini = arr[i];
+        int idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < mini)
+            {
+                mini = arr[j];
+                idx = j;
             }
         }
+        swap(arr[i], arr[idx]);
     }
 }
-int main(){
-    int arr[]={5,3,4,6,7,1,0};
+int main()
+{
+    int arr[] = {5, 3, 4, 6, 7, 1, 0};
     int n = 7;
-    selectionSort(arr,n);
-    for(int ele:arr){
-        cout<<ele<<" ";
+    selectionSort(arr, n);
+    for (int ele : arr)
+    {
+        cout << ele << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
