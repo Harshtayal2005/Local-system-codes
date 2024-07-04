a2ddbcaa8e76eaf5d44804f6854aa583055f1618
Harsh Tayal <harshtayal2005@gmail.com>
2024-07-04 18:53:48 +0530

    new snippet added

---
+++ Snippets/Trie.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node
+{
+public:
+    Node *links[26];
+    bool flag = false;
+    bool containsKey(char ch)
+    {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
+    }
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+    void setEnd()
+    {
+        flag = true;
+    }
+    bool getEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    /** Initialize your data structure here. */
+    Trie()
+    {
+        root = new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node()); // add this word to node
+            }
+            node = node->get(word[i]); // move to next
+        }
+        node->setEnd();
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+                return false;
+            node = node->get(word[i]);
+            if (node == NULL)
+                return false;
+        }
+        return node->getEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix)
+    {
+        Node *node = root;
+        for (int i = 0; i < prefix.length(); i++)
+        {
+            if (!node->containsKey(prefix[i]))
+                return false;
+            node = node->get(prefix[i]);
+            if (node == NULL)
+                return false;
+        }
+        return true;
+    }
+};
